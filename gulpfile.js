ae726f9e1f9fd0dfd0fb367afbf7aa3b977372aa
Kaylea Britton <kbbritton217@gmail.com>
2017-09-07 15:43:49 -0400

    bug fix

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var javascriptFiles = [
+	'./app.js',
+	'./workouts/define.js',
+	'./workouts/log.js',
+	'./user/auth.js'
+];
+
+gulp.task('bundle', function() {
+	return gulp.src(javascriptFiles)
+		.pipe(sourcemaps.init())
+		.pipe(concat('bundle.js')) // squash files together into bundle.js
+		.pipe(uglify())
+		.pipe(sourcemaps.write('./maps/'))
+		.pipe(gulp.dest('./dist'));// save into /dist folder
+});
+
+
+
+gulp.task('default', ['bundle']);
